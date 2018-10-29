6164acdbd3200c8213c254561e5e549a2d765b2e
nhforman <nhforman@usc.edu>
2018-10-28 19:59:31 -0700

    Dictionary and Square Instantiation

--- MakersSimulator.py
+++ MakersSimulator.py
@@ -21,7 +21,7 @@ class Square:
         15 : "top-left-bottom-right"
     }
 
-    def _init_(row, column, top=None, bottom=None, left=None, right=None):
+    def _init_(self, row, column, top=None, left=None, bottom=None, right=None):
         self.top = top
         self.bottom = bottom
         self.left = left
@@ -32,5 +32,43 @@ class Square:
 
 class Maze:
 
-    def _init_(size):
+    def _init_(self, size):
         self.size = size
+
+    def processFile(self) :
+        f = open("input.txt","r")
+        print (f.read())
+
+    def createSquare(self, type, row, column) :
+        format = dictionary[type]
+        top, left, bottom, right = 0,0,0,0
+        if (format.contains("top")) :
+            top = 1
+        if (format.contains("left")) :
+            left = 1
+        if (format.contains("bottom")) :
+            bottom = 1
+        if (format.contains("right")) :
+            right = 1
+        return Square(row, column, top, left, bottom, right)
+
+# public static void main (int argc, const char* argv[]) {}
+maze = Maze()
+maze.processFile()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# qwertyuiop
